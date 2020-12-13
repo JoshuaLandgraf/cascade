c53af15c47b8dbf8761c8ff395e578ae3a271784
Joshua Landgraf <jland@cs.utexas.edu>
2020-12-13 14:05:11 +0000

    Fixes for executing from install directory

--- share/cascade/f1/daemon/start.sh
+++ share/cascade/f1/daemon/start.sh
@@ -6,11 +6,12 @@ export AWS_FPGA=$HOME_DIR/aws-fpga
 export BUILD_DIR=$(dirname $(readlink -f $0))
 
 # Setup SDK env
-source $AWS_FPGA/sdk_setup.sh >/dev/null 2>&1
-rm -f awsver.txt
+#source $AWS_FPGA/sdk_setup.sh >/dev/null 2>&1
+#rm -f awsver.txt
 
 cd $BUILD_DIR
-make
+#make
+sudo g++ -O3 -std=c++11 -fpermissive -DCONFIG_LOGLEVEL=4 -g -Wall -I $AWS_FPGA/sdk/userspace/include -I $AWS_FPGA/hdk/common/software/include  -lfpga_mgmt -lrt -lpthread $AWS_FPGA/sdk/userspace/utils/sh_dpi_tasks.c aos_daemon.c -o daemon
 RC=$?
 
 if [ $RC -ne 0 ]; then
